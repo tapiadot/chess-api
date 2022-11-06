0cf959f3c482d0f2090b23b92dd608ac1b28e625
Luis Tapia <devlou@protonmail.com>
2022-11-06 16:50:04 -0500

    Create images with JavaScript

--- src/index.js
+++ src/index.js
@@ -1,6 +1,49 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+"use strict";
 
-console.log('Happy hacking :)')
+const puzzlesNode = document.querySelector("#puzzles");
+
+async function getRandomPuzzle() {
+  const response = await fetch("https://api.chess.com/pub/puzzle/random");
+  return await response.json();
+}
+
+async function getPuzzleProp(puzzle, prop) {
+  const response = await puzzle;
+  return response[prop];
+}
+
+async function displayPuzzles(num, delay) {
+  let counter = 0;
+  const loop = () => {
+    setTimeout(async () => {
+      // puzzle info
+      const puzzle = await getRandomPuzzle();
+      const puzzleImg = await getPuzzleProp(puzzle, "image");
+      const puzzleDesc = await getPuzzleProp(puzzle, "title");
+
+      // dom
+      const div = document.createElement("div");
+      const p = document.createElement("p");
+      const img = document.createElement("img");
+      puzzlesNode.appendChild(div);
+      div.appendChild(p);
+      div.appendChild(img);
+
+      // add info
+      div.classList.add("p-4");
+      p.textContent = puzzleDesc;
+      img.src = puzzleImg;
+      img.alt = "Random daily puzzle from chess.com";
+      img.classList.add("mx-auto");
+
+      counter++;
+      if (counter < num) {
+        loop();
+      }
+    }, delay);
+  };
+  loop();
+}
+
+displayPuzzles(1, 0);
+displayPuzzles(20, 15000);
