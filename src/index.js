546585a4a73a6d829093b22f7267db684bf27ddd
Luis Tapia <devlou@protonmail.com>
2022-11-10 00:04:34 -0500

    Display random streamer when clicking button

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 "use strict";
 
+const streamersNode = document.querySelector("#streamers");
+const btnGetStreamer = document.querySelector("#get-streamer");
 const puzzlesNode = document.querySelector("#puzzles");
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min);
+}
 
 async function getRandomPuzzle() {
   const response = await fetch("https://api.chess.com/pub/puzzle/random");
@@ -12,19 +20,52 @@ async function getPuzzleProp(puzzle, prop) {
   return response[prop];
 }
 
-function createImageNode(src, alt) {
+async function getStreamerList() {
+  const response = await fetch("https://api.chess.com/pub/streamers");
+  return await response.json();
+}
+
+async function getRandomStreamer() {
+  const response = await getStreamerList();
+  const streamers = response.streamers;
+  const ran = getRandomInt(0, streamers.length);
+  return streamers[ran];
+}
+
+function createImageNode(src, alt, width) {
   const container = document.createElement("div");
   container.classList.add("p-4");
 
   const image = document.createElement("img");
   image.classList.add("mx-auto");
-  image.width = "400";
+  image.width = width;
   image.src = src;
   image.alt = alt;
 
   container.appendChild(image);
 
   return container;
+}
+
+async function displayRandomStreamer() {
+  const streamer = await getRandomStreamer();
+  // data
+  const avatar = streamer.avatar;
+  const username = streamer.username;
+  const url = streamer.url;
+
+  // dom
+  const div = document.createElement("div");
+  const a = document.createElement("a");
+  a.href = url;
+  a.target = "_blank";
+  const img = createImageNode(avatar, `${username}'s Chess.com profile`, 100);
+  const p = document.createElement("p");
+  p.textContent = username;
+  streamersNode.appendChild(div);
+  div.appendChild(a);
+  a.appendChild(img);
+  div.appendChild(p);
 }
 
 async function displayPuzzles(num, delay) {
@@ -39,7 +80,8 @@ async function displayPuzzles(num, delay) {
       // img
       const image = createImageNode(
         puzzleImg,
-        "Random daily puzzle from chess.com"
+        "Random daily puzzle from chess.com",
+        400
       );
       puzzlesNode.appendChild(image);
 
@@ -57,5 +99,8 @@ async function displayPuzzles(num, delay) {
   loop();
 }
 
+btnGetStreamer.addEventListener("click", displayRandomStreamer);
+
+displayRandomStreamer();
 displayPuzzles(1, 0);
-displayPuzzles(20, 15000);
+displayPuzzles(50, 15000);
